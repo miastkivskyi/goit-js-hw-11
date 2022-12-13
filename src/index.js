d1801fd68aaeaaa3cbd6b6a63a146ab754d1d8ab
Rodion Myastkowskuy <rodion.myastkowskuy@gmail.com>
2022-12-13 11:50:40 +0200

    adds pagination and comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
+import { Notify } from 'notiflix';
 import templateFunction from './template.hbs';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import './css/styles.css';
 
 const refs = {
@@ -8,19 +11,39 @@ const refs = {
   formData: document.querySelector('#search-form'),
   input: document.querySelector('input'),
 };
+const simple = new SimpleLightbox('.gallery a');
+
 let searchQuery = '';
+let pages = 1;
 
 refs.formData.addEventListener('submit', addNewSearch);
 
 function addNewSearch(e) {
   e.preventDefault();
   searchQuery = refs.input.value;
-  console.log(searchQuery);
-
-  findSimilarPhotos(searchQuery).then(renderCard);
+  nullPage();
+  paginationOfImg(searchQuery);
 }
 
-async function findSimilarPhotos() {
+window.addEventListener('scroll', () => {
+  if (
+    window.scrollY + window.innerHeight >=
+    document.documentElement.scrollHeight
+  ) {
+    paginationOfImg();
+  }
+});
+
+async function paginationOfImg() {
+  try {
+    const response = await GetMorePhoto();
+    return renderCard(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function GetMorePhoto() {
   try {
     const res = await axios({
       url: 'https://pixabay.com/api/',
@@ -31,14 +54,40 @@ async function findSimilarPhotos() {
         orientation: 'horizontal',
         safesearch: true,
         per_page: 40,
+        page: pages,
       },
     });
+    Comment(res.data.totalHits);
+    upPage();
     return res.data.hits;
   } catch (e) {
     return console.error(e);
   }
 }
 
+function Comment(value) {
+  if (pages === 1 && value !== 0) {
+    Notify.success(`Hooray! We found ${value} images.`);
+  }
+  if (value === 0) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  } else if (value / 40 + 1 < pages) {
+    Notify.info("We're sorry, but you've reached the end of search results.");
+  }
+}
+
 function renderCard(array) {
   refs.galleryEl.insertAdjacentHTML('beforeend', templateFunction(array));
+  simple.refresh();
+}
+
+function upPage() {
+  pages += 1;
+}
+
+function nullPage() {
+  pages = 1;
+  refs.galleryEl.innerHTML = '';
 }
